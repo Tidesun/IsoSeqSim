d5f4f5da047ed113573b411943581f9b63f09192
Haoran Li <lihaoran9836@gmail.com>
2021-02-06 10:28:47 +0800

    modify to accept expression

--- utilities/py_isoseqsim_generate_expr_matrix.py
+++ utilities/py_isoseqsim_generate_expr_matrix.py
@@ -5,27 +5,36 @@ import numpy as np
 def main(args):
 	sys.stdout.write("Start analysis: " + time.strftime("%a,%d %b %Y %H:%M:%S") + "\n")
 	sys.stdout.flush()
-	generate_expr_matrix(args.input,args.nb_n,args.nb_p,args.output)
+	generate_expr_matrix(args.input,args.expression,args.nb_n,args.nb_p,args.output)
 	sys.stdout.write("Finish analysis: " + time.strftime("%a,%d %b %Y %H:%M:%S") + "\n")
 	sys.stdout.flush()
 
-def generate_expr_matrix(input_gpd_fl,nb_r,nb_p,output_expr_mtx):
+def generate_expr_matrix(input_gpd_fl,expression_fl,nb_r,nb_p,output_expr_mtx):
 	# parse gpd file
-	gpd_list = []
+	gpd_dict = {}
 	for line in input_gpd_fl:
-		gpd_list.append(line.strip())
-	# generate random read count based on negative binomial distribution
-	nb_list = np.random.negative_binomial(nb_r,nb_p,len(gpd_list)).tolist()
-	i = 0
-	for gpd in gpd_list:
-		print >>output_expr_mtx, gpd + "\t" + str(nb_list[i])
-		i += 1
+		isoform_name = line.split('\t')[1]
+		gpd_dict[isoform_name] = {'line':line.strip()}
+	nb_list = []
+	for line in expression_fl:
+		[isoform_name,count] = line.split('\t')
+		gpd_dict[isoform_name]['count'] = count
+	for gpd in gpd_dict:
+		print >>output_expr_mtx, gpd['line'] + "\t" + gpd['count']
+	# # generate random read count based on negative binomial distribution
+	# nb_list = np.random.negative_binomial(nb_r,nb_p,len(gpd_list)).tolist()
+	# i = 0
+	# for gpd in gpd_list:
+	# 	print >>output_expr_mtx, gpd + "\t" + str(nb_list[i])
+	# 	i += 1
 	input_gpd_fl.close()
+	expression_fl.close()
 	output_expr_mtx.close()
 
 def do_inputs():
 	parser = argparse.ArgumentParser(description="Randomly generate read count for each isoform based on negative binomial (NB) distribution. Read count is shown in last column of output file", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-i','--input',type=argparse.FileType('r'),required=True,help="Input: gpd file")
+	parser.add_argument('-expr','--expression',type=argparse.FileType('r'),required=True,help="Input: expression file")
 	parser.add_argument('-n','--nb_n',type=int,default=10,help="Parameter of the NB distribution, n")
 	parser.add_argument('-p','--nb_p',type=float,default=0.5,help="Parameter of the NB distribution, p")
 	parser.add_argument('-o','--output',type=argparse.FileType('w'),required=True,help="Output: gpd + read count file")
